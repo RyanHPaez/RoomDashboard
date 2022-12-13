3bb09ae6be36b2e2d6fd32728710e62af4b8f74e
ryanhpaez <solosnail@gmail.com>
2022-12-12 21:57:09 -0800

    navitems css

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,55 +1,49 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 import Button from "@mui/material/Button";
 import Grid from "@mui/material/Grid";
-
 
 function Navigation() {
   return (
     <div className="top">
       <div className="navigation">
-      <Grid container spacing={2}>
-      <Grid item xs={2} md={4.5}></Grid>
-      <Grid item xs={7} md={5}>
-        <p>Room DashBoard</p>
-        </Grid>
-        <Grid item xs={3} md={1.75} container justifyContent="flex-end">
-        <Button id="AN"
-         
-              variant="contained"              
-              style={{ borderRadius: 60 }}             
-            >             
+        <Grid container spacing={2}>
+          <Grid item xs={2} md={4.5}></Grid>
+          <Grid item xs={7} md={5}>
+            <p>Room DashBoard</p>
+          </Grid>
+          <Grid item xs={3} md={1.75} container justifyContent="flex-end">
+            <Button id="AN" variant="contained" style={{ borderRadius: 60 }}>
               AN
             </Button>
-        </Grid>
+          </Grid>
         </Grid>
       </div>
       <div className="navButons">
         <Grid container spacing={2}>
           <Grid item xs={3} md={3}>
-            <Button style={{ backgroundColor: 'transparent' }}>
+            <Button style={{ backgroundColor: "transparent" }}>
               <NavLink className="nav-link" to="/Home">
-                <i className="NavItems" style={{ fontSize: "15px" }}>
+                <p className="NavItems" >
                   Home
-                </i>
+                </p>
               </NavLink>
             </Button>
           </Grid>
           <Grid item xs={3} md={3}>
-            <Button  style={{ backgroundColor: 'transparent' }}>
+            <Button style={{ backgroundColor: "transparent" }}>
               <NavLink className="nav-link" to="/Room2">
-                <i className="NavItems" style={{ fontSize: "15px" }}>
+                <p className="NavItems">
                   API DATA
-                </i>
+                </p>
               </NavLink>
             </Button>
           </Grid>
-          <Grid item xs={3} md={1}>
-            <Button  style={{ backgroundColor: 'transparent' }}>
+          <Grid item xs={3} md={1.25}>
+            <Button style={{ backgroundColor: "transparent" }}>
               <NavLink className="nav-link" to="/Room3">
-                <i className="NavItems" style={{ fontSize: "15px" }}>
+                <p className="NavItems">
                   Room 3
-                </i>
+                </p>
               </NavLink>
             </Button>
           </Grid>
